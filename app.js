33f9929d98054daf30e11865972cdb8797d280f8
yarocruz <yarocruz@gmail.com>
2020-03-12 21:29:12 -0400

    finished adding questions for all employee types. added conditions that lets you keep adding employees as needed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -15,23 +15,132 @@ const render = require("./lib/htmlRenderer");
 
 // Initial inquirer prompt to determine employee type
 
-inquirer.prompt([
-    {
-        type: 'list',
-        message: 'What type of employee?',
-        name: 'employeeType',
-        choices: [Manager, Engineer, Intern]
-    },
-])
-    .then(answer => {
-        console.log(answer);
-        if (answer.employeeType === 'Manager') {
-            managerQuestions();
-        } else {
-            console.log('Done!');
-            return;
-        }
-    })
+let employeeArr = [];
+
+const initialQuestion = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What type of employee?',
+            name: 'employeeType',
+            choices: [Manager, Engineer, Intern]
+        },
+    ])
+        .then(answer => {
+            console.log(answer);
+            if (answer.employeeType === 'Manager') {
+                managerQuestions();
+            } else if
+                (answer.employeeType === 'Engineer') {
+                engineerQuestions();
+            } else if
+                (answer.employeeType === 'Intern') {
+                internQuestions();
+            }
+            else {
+                console.log('Done!');
+                return;
+            }
+        })
+}
+
+initialQuestion();
+
+const internQuestions = () => {
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'What is the interns name?',
+            name: 'internName'
+        },
+        {
+            type: 'input',
+            message: 'What is the interns employee id?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'What is the interns email?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the interns school?',
+            name: 'school',
+        },
+        {
+            type: 'confirm',
+            message: 'Do you need to enter another employee?',
+            name: 'addCheck',
+        },
+
+    ])
+        .then(answers => {
+            const intern = new Intern(answers.internName, answers.id, answers.email, answers.school);
+            employeeArr.push(intern);
+
+            console.log(employeeArr);
+
+            if (answers.addCheck) {
+                initialQuestion();
+            } else {
+                let data = render(employeeArr);
+                fs.writeFile(outputPath, data, (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                });
+            }
+        })
+}
+
+const engineerQuestions = () => {
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'What is the engineers name?',
+            name: 'engineerName'
+        },
+        {
+            type: 'input',
+            message: 'What is engineers employee id?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'What is the engineers email?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the engineers github username?',
+            name: 'github',
+        },
+        {
+            type: 'confirm',
+            message: 'Do you need to enter another employee?',
+            name: 'addCheck',
+        },
+
+    ])
+        .then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.id, answers.email, answers.github);
+            employeeArr.push(engineer);
+
+            console.log(employeeArr);
+
+            if (answers.addCheck) {
+                initialQuestion();
+            } else {
+                let data = render(employeeArr);
+                fs.writeFile(outputPath, data, (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                });
+            }
+        })
+}
 
 const managerQuestions = () => {
     inquirer.prompt([
@@ -59,32 +168,36 @@ const managerQuestions = () => {
         {
             type: 'confirm',
             message: 'Do you need to enter another employee?',
-            name: 'officeNumber',
+            name: 'addCheck',
         },
 
     ])
         .then(answers => {
-            console.log(answers.managerName);
 
             // and to create objects for each team member (using the correct classes as blueprints!)
             const manager = new Manager(answers.managerName, answers.id, answers.email, answers.officeNumber);
-            console.log(manager);
+            employeeArr.push(manager);
 
             // After the user has input all employees desired, call the `render` function (required
             // above) and pass in an array containing all employee objects; the `render` function will
             // generate and return a block of HTML including templated divs for each employee!
-            let employeeArr = [manager]
+
             console.log(employeeArr);
-            let data = render(employeeArr);
-            console.log(data);
+
+            if (answers.addCheck) {
+                initialQuestion();
+            } else {
+                let data = render(employeeArr);
+                fs.writeFile(outputPath, data, (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                });
+            }
 
             // After you have your html, you're now ready to create an HTML file using the HTML
             // returned from the `render` function. Now write it to a file named `team.html` in the
             // `output` folder. You can use the variable `outputPath` above target this location.
-            fs.writeFile(outputPath, data, (err) => {
-                if (err) throw err;
-                console.log('The file has been saved!');
-            });
+
         })
 }
 
